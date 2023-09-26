8e0e5939a466b938dfef87323736c3b2aa6b4e8a
Krzysztof Radzięta <kustosz142@gmail.com>
2023-09-26 19:31:17 +0200

    api.php how tu put data in view

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// every route in api.php starts by api/
+Route::get('/posts', function () {
+    return response()->json([
+        'posts' => [
+            [
+                'title' => 'Post'
+            ]
+        ]
+    ]);
+});
--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get("/hello", function () {
-    return response("<h1>Hello</h1>", 300)
-    ->header('Content-Type', 'text/plain')
-    ->header('foo', 'bar');
-});
-
-Route::get("/posts/{id}", function($id){
-    // dd($id); // Dump and Die - it will stop everything and show data
-    // ddd($id); // Dump Debbug Die - special debbug view
-    return response('Post ' . $id);
-})->where('id', '[0-9]+'); // name of parameter, regular expression, what this parametert should be 
-
-Route::get('/search', function(Request $request){ // right click, then import class
-    // dd($request);
-    // dd("Name: " . $request->name . " City: " . $request->city);
-    return  $request->name . ' ' . $request->city;
+    return view('listings', [
+        'heading' => "Latest Listings"
+    ]);
 });
