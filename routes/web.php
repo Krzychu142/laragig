971e53dbbb25d5131c7f8bc75e01d65806cb9623
Krzysztof Radzięta <kustosz142@gmail.com>
2023-09-26 18:27:30 +0200

    19:16

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/hello", function () {
+    return response("<h1>Hello</h1>", 300)
+    ->header('Content-Type', 'text/plain')
+    ->header('foo', 'bar');
+});
