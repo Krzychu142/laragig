e93c26c648c89292580fb5defb26bb55d3427ed9
Krzysztof Radzięta <kustosz142@gmail.com>
2023-09-26 19:02:26 +0200

    26:17

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,16 @@ Route::get("/hello", function () {
     return response("<h1>Hello</h1>", 300)
     ->header('Content-Type', 'text/plain')
     ->header('foo', 'bar');
+});
+
+Route::get("/posts/{id}", function($id){
+    // dd($id); // Dump and Die - it will stop everything and show data
+    // ddd($id); // Dump Debbug Die - special debbug view
+    return response('Post ' . $id);
+})->where('id', '[0-9]+'); // name of parameter, regular expression, what this parametert should be 
+
+Route::get('/search', function(Request $request){ // right click, then import class
+    // dd($request);
+    // dd("Name: " . $request->name . " City: " . $request->city);
+    return  $request->name . ' ' . $request->city;
 });
